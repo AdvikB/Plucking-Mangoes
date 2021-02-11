64795828975eb66fc8198591314ad94777e0b83d
AdvikB <73247534+AdvikB@users.noreply.github.com>
2021-02-10 20:45:10 -0600

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,10 @@
+class Stone{
+    constructor(){
+        var options = {
+            isStatic:false,
+            restitution:0,
+            density:1,
+            friction:1.2
+        }
+    }
+}
---
+++ tree.js
@@ -0,0 +1,10 @@
+class Tree{
+    constructor(){
+        var options = {
+            isStatic:false,
+            friction:1,
+            density:1.2
+        }
+      
+    }
+}
